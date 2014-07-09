b8f506fb2a363694aa2e6dd086759c788ea6a0bb
Jernej Kovacic <jkovacic@gmail.com>
2014-07-09 20:33:13 +0200

    First commit

---
+++ FreeRTOS/Source/portable/GCC/tm4c123g/portmacro.h
@@ -0,0 +1,206 @@
+/*
+ * It turns out that portmacro.h from the officially supported
+ * GCC/ARM_CM4F port can be reused at TI TM4C123G* too.
+ * The following minor changes were applied:
+ * - all remaining occurrences of standard C integer types (e.g. int, short,
+ *   long, etc.) were replaced by their stdint.h alternatives
+ * - some unnecessary constants were removed as the functionality where they
+ *   were used is already implemented in generic drivers
+ * - all "annoying" tabs have been replaced by spaces
+ *
+ * The original file is available under the following license:
+ */
+
+/*
+    FreeRTOS V8.0.1 - Copyright (C) 2014 Real Time Engineers Ltd.
+    All rights reserved
+
+    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS provides completely free yet professionally developed,    *
+     *    robust, strictly quality controlled, supported, and cross          *
+     *    platform software that has become a de facto standard.             *
+     *                                                                       *
+     *    Help yourself get started quickly and support the FreeRTOS         *
+     *    project by purchasing a FreeRTOS tutorial book, reference          *
+     *    manual, or both from: http://www.FreeRTOS.org/Documentation        *
+     *                                                                       *
+     *    Thank you!                                                         *
+     *                                                                       *
+    ***************************************************************************
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation >>!AND MODIFIED BY!<< the FreeRTOS exception.
+
+    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
+    >>!   distribute a combined work that includes FreeRTOS without being   !<<
+    >>!   obliged to provide the source code for proprietary components     !<<
+    >>!   outside of the FreeRTOS kernel.                                   !<<
+
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  Full license text is available from the following
+    link: http://www.freertos.org/a00114.html
+
+    1 tab == 4 spaces!
+
+    ***************************************************************************
+     *                                                                       *
+     *    Having a problem?  Start by reading the FAQ "My application does   *
+     *    not run, what could be wrong?"                                     *
+     *                                                                       *
+     *    http://www.FreeRTOS.org/FAQHelp.html                               *
+     *                                                                       *
+    ***************************************************************************
+
+    http://www.FreeRTOS.org - Documentation, books, training, latest versions,
+    license and Real Time Engineers Ltd. contact details.
+
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
+    compatible FAT file system, and our tiny thread aware UDP/IP stack.
+
+    http://www.OpenRTOS.com - Real Time Engineers ltd license FreeRTOS to High
+    Integrity Systems to sell under the OpenRTOS brand.  Low cost OpenRTOS
+    licenses offer ticketed support, indemnification and middleware.
+
+    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
+    engineered and independently SIL3 certified version for use in safety and
+    mission critical applications that require provable dependability.
+
+    1 tab == 4 spaces!
+*/
+
+
+#ifndef _PORTMACRO_H_
+#define _PORTMACRO_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*-----------------------------------------------------------
+ * Port specific definitions.
+ *
+ * The settings in this file configure FreeRTOS correctly for the
+ * given hardware and compiler.
+ *
+ * These settings should not be altered.
+ *-----------------------------------------------------------
+ */
+
+/* Type definitions. */
+#define portCHAR            char
+#define portFLOAT           float
+#define portDOUBLE          double
+#define portLONG            int32_t
+#define portSHORT           int16_t
+#define portSTACK_TYPE      uint32_t
+#define portBASE_TYPE       portLONG
+
+typedef portSTACK_TYPE StackType_t;
+typedef int32_t BaseType_t;
+typedef uint32_t UBaseType_t;
+
+#if( configUSE_16_BIT_TICKS == 1 )
+    typedef uint16_t TickType_t;
+    #define portMAX_DELAY ( TickType_t )     0xffff
+#else
+    typedef uint32_t TickType_t;
+    #define portMAX_DELAY ( TickType_t )     0xffffffffUL
+#endif
+/*-----------------------------------------------------------*/
+
+/* Architecture specifics. */
+#define portSTACK_GROWTH            ( -1 )
+#define portTICK_PERIOD_MS          ( ( TickType_t ) 1000 / configTICK_RATE_HZ )
+#define portBYTE_ALIGNMENT          8
+/*-----------------------------------------------------------*/
+
+
+/* Scheduler utilities. */
+extern void vPortYield( void );
+#define portYIELD()                 vPortYield()
+#define portEND_SWITCHING_ISR( xSwitchRequired ) \
+    if( xSwitchRequired ) portNVIC_INT_CTRL_REG = portNVIC_PENDSVSET_BIT
+#define portYIELD_FROM_ISR( x )     portEND_SWITCHING_ISR( x )
+/*-----------------------------------------------------------*/
+
+
+/* Critical section management. */
+extern void vPortEnterCritical( void );
+extern void vPortExitCritical( void );
+extern uint32_t ulPortSetInterruptMask( void );
+extern void vPortClearInterruptMask( uint32_t ulNewMaskValue );
+
+#define portSET_INTERRUPT_MASK_FROM_ISR()       ulPortSetInterruptMask()
+#define portCLEAR_INTERRUPT_MASK_FROM_ISR(x)    vPortClearInterruptMask(x)
+#define portDISABLE_INTERRUPTS()                ulPortSetInterruptMask()
+#define portENABLE_INTERRUPTS()                 vPortClearInterruptMask(0)
+#define portENTER_CRITICAL()                    vPortEnterCritical()
+#define portEXIT_CRITICAL()                     vPortExitCritical()
+/*-----------------------------------------------------------*/
+
+
+/* Task function macros as described on the FreeRTOS.org WEB site.  These are
+not necessary for to use this port.  They are defined so the common demo files
+(which build with all the ports) will build. */
+#define portTASK_FUNCTION_PROTO( vFunction, pvParameters ) void vFunction( void *pvParameters )
+#define portTASK_FUNCTION( vFunction, pvParameters ) void vFunction( void *pvParameters )
+/*-----------------------------------------------------------*/
+
+/* Tickless idle/low power functionality. */
+#ifndef portSUPPRESS_TICKS_AND_SLEEP
+    extern void vPortSuppressTicksAndSleep( TickType_t xExpectedIdleTime );
+    #define portSUPPRESS_TICKS_AND_SLEEP( xExpectedIdleTime ) vPortSuppressTicksAndSleep( xExpectedIdleTime )
+#endif
+/*-----------------------------------------------------------*/
+
+/* Architecture specific optimisations. */
+#if configUSE_PORT_OPTIMISED_TASK_SELECTION == 1
+
+    /* Generic helper function. */
+    __attribute__( ( always_inline ) ) static inline uint8_t ucPortCountLeadingZeros( uint32_t ulBitmap )
+    {
+    uint8_t ucReturn;
+
+        __asm volatile ( "clz %0, %1" : "=r" ( ucReturn ) : "r" ( ulBitmap ) );
+        return ucReturn;
+    }
+
+    /* Check the configuration. */
+    #if( configMAX_PRIORITIES > 32 )
+        #error configUSE_PORT_OPTIMISED_TASK_SELECTION can only be set to 1 when configMAX_PRIORITIES is less than or equal to 32.  It is very rare that a system requires more than 10 to 15 difference priorities as tasks that share a priority will time slice.
+    #endif
+
+    /* Store/clear the ready priorities in a bit map. */
+    #define portRECORD_READY_PRIORITY( uxPriority, uxReadyPriorities ) ( uxReadyPriorities ) |= ( 1UL << ( uxPriority ) )
+    #define portRESET_READY_PRIORITY( uxPriority, uxReadyPriorities ) ( uxReadyPriorities ) &= ~( 1UL << ( uxPriority ) )
+
+    /*-----------------------------------------------------------*/
+
+    #define portGET_HIGHEST_PRIORITY( uxTopPriority, uxReadyPriorities ) uxTopPriority = ( 31 - ucPortCountLeadingZeros( ( uxReadyPriorities ) ) )
+
+#endif /* configUSE_PORT_OPTIMISED_TASK_SELECTION */
+
+/*-----------------------------------------------------------*/
+
+#ifdef configASSERT
+    void vPortValidateInterruptPriority( void );
+    #define portASSERT_IF_INTERRUPT_PRIORITY_INVALID() 	vPortValidateInterruptPriority()
+#endif
+
+/* portNOP() is not required by this port. */
+#define portNOP()
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif  /* _PORTMACRO_H_ */
---
+++ README.md
@@ -0,0 +1,83 @@
+##About
+[FreeRTOS](http://www.freertos.org/), ported to the
+[Texas Instruments TM4C123GLX Launchpad](http://www.ti.com/tool/ek-tm4c123gxl), 
+i.e. an evaluation board with the 
+[TI TM4C123GH6PM](http://www.ti.com/lit/ds/symlink/tm4c123gh6pm.pdf)
+microcontroller, based on ARM&#xae; Cortex-M4F.
+
+The current version is based on FreeRTOS 8.0.1. The port will be regularly
+updated with newer versions of FreeRTOS when they are released.
+
+The port is still at an early development stage and includes only very basic
+demo tasks. More complex tasks will be included in the future.
+
+
+##Prerequisites
+* _Tiva&#x2122; C series TM4C123GLX Launchpad_
+* A _Micro-B USB cable_, usually shipped with a Launchpad
+* _Sourcery CodeBench Lite Edition for ARM EABI_ toolchain (now owned by Mentor Graphics),
+based on GCC. See comments in _setenv.sh_ for more details about download and installation.
+* _GNU Make_
+* _[LM4Tools](https://github.com/utzig/lm4tools)_ or 
+_[TI LMFlash Programmer](http://www.ti.com/tool/lmflashprogrammer)_ 
+to upload images to the Launchpad
+* Optionally _[OpenOCD](http://openocd.sourceforge.net/)_ for deugging.
+See comments in _start\_openocd.sh_ for more details about installation.
+* Optionally a _FTDI or PL2303HX cable supporting +3.3V based TTL level UART signals_
+
+##Build
+A convenience Bash script _setenv.sh_ is provided to set paths to toolchain's commands
+and libraries. You may edit it and adjust the paths according to your setup. To set up
+the necessary paths, simply type:
+
+`. ./setenv.sh`
+
+To build the image with the test application, just run _make_ or _make rebuild_.
+If the build process is successful, the image file _image.bin_ will be ready to
+upload to the Launchpad.
+
+##Run
+When the image _tiva.bin_ is successfully built, you may upload it to
+the Launchpad, using the simple cross platform CLI tool 
+[LM4Tools](https://github.com/utzig/lm4tools):
+
+`/path/to/lm4flash tiva.bin`
+
+Alternatively you may use the GUI tool 
+[TI LMFlash Programmer](http://www.ti.com/tool/lmflashprogrammer), provided
+by Texas Instruments. It is available for Windows only.
+
+To establish the first serial connection, just open a serial terminal program 
+(e.g. _Hyper Terminal_, _GtkTerm_ or _Minicom_)
+and configure the FTDI virtual COM port to 115200 bps, 8 data bits, no parity,
+1 stop bit. 
+
+To establish the second serial connection, connect the FTDI or PL2303HX cable's
+TX connection to pin B0, its RX connection to pin B1 and the GND connection to
+the nearby GND pin. Then open another instance of a serial terminal and configure
+the cable's virtual COM port with the same settings as at the first connection. 
+If you do not have a FTDI or PL2303HX cable, you may open `app/FreeRTOSConfig.h`,
+set `APP_PRINT_UART_NR` and `APP_RECV_UART_NR` both to 0 and rebuild the application.
+In this case, it is not necessary to establish the second connection
+as the entire communication will be performed by the first one.
+
+##Application
+The first serial connection is a debug connection, intended to
+display diagnostic messages only. It will display a welcome message and
+start printing the system's uptime.
+
+The second serial connection receives characters you send using a keyboard but
+does not display anything until _Enter_ is pressed. When this happens, it 
+will invert your text and print it.
+
+In future, the application will be expanded to use built-in switches and
+RGB LED diodes.
+
+##License
+All source and header files in FreeRTOS/ and its subdirectiories are licensed under
+the [modified GPL license](http://www.freertos.org/license.txt).
+All other files that are not derived from the FreeRTOS source distribution are licensed
+under the [Apache 2.0 license](http://www.apache.org/licenses/LICENSE-2.0).
+
+For the avoidance of any doubt refer to the comment included at the top of each source and
+header file for license and copyright information.
